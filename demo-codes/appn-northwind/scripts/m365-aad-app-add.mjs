9b765ffc615711394ae27719ab8de9ae9aa2402a
Arjun Menon <arjun.umenon@gmail.com>
2024-02-13 17:42:38 +0530

    Working version in local

--- demo-codes/appn-northwind/scripts/m365-aad-app-add.mjs
+++ demo-codes/appn-northwind/scripts/m365-aad-app-add.mjs
@@ -10,7 +10,7 @@ async function createAADApp(appName, apiPermissions, exposeAPIURL) {
     let createdApp = null;
     try {
         // Create the Azure AD app
-        createdApp = JSON.parse(await $`m365 aad app add --name ${appName} --apisDelegated ${apiPermissions}  --redirectUris 'https://login.microsoftonline.com/common/oauth2/nativeclient, https://${exposeAPIURL}' --platform spa --grantAdminConsent  --multitenant --withSecret --uri api://${exposeAPIURL}/_appId_ --scopeName access_as_user --scopeAdminConsentDescription 'Access the application as Logged in User' --scopeAdminConsentDisplayName 'Access as the logged in User' --scopeConsentBy admins --output json`);
+        createdApp = JSON.parse(await $`m365 entra app add --name ${appName} --apisDelegated ${apiPermissions}  --redirectUris 'https://login.microsoftonline.com/common/oauth2/nativeclient, https://${exposeAPIURL}' --platform spa --grantAdminConsent  --multitenant --withSecret --uri api://${exposeAPIURL}/_appId_ --scopeName access_as_user --scopeAdminConsentDescription 'Access the application as Logged in User' --scopeAdminConsentDisplayName 'Access as the logged in User' --scopeConsentBy admins --withSecret --save --output json`);
     }
     catch (err) {
         console.error(`  ${chalk.red(err.stderr)}`);
@@ -21,7 +21,7 @@ async function createAADApp(appName, apiPermissions, exposeAPIURL) {
 // For Production
 // const appName = 'Teams App Camp App June3-V1';
 //For Development
-const appName = 'Teams App Camp App June3-V1-DEV';
+const appName = 'Exports Live India - 2024 - AAD';
 const apiPermissions = 'https://graph.microsoft.com/User.Read';
 
 // //For Development
