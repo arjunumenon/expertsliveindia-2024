8ff84754617e10478bb1753fcb0f79aa509cbbe9
Arjun Menon <arjun.umenon@gmail.com>
2024-02-13 17:21:35 +0530

    Initial Commit - Application

---
+++ demo-codes/appn-northwind/4-scripts/m365-aad-app-add.mjs
@@ -0,0 +1,32 @@
+#!/usr/bin/env zx
+
+//Check if Windows Terminal. If Windows Terminal, set the shell to bash.exe
+if (!!process.env.WT_SESSION) {
+    $.shell = `C:/Program Files/Git/usr/bin/bash.exe`;
+}
+
+async function createAADApp(appName, apiPermissions, exposeAPIURL) {
+    console.log(`Creating Azure AD app ${appName}...`);
+    let createdApp = null;
+    try {
+        // Create the Azure AD app
+        createdApp = JSON.parse(await $`m365 aad app add --name ${appName} --apisDelegated ${apiPermissions}  --redirectUris 'https://login.microsoftonline.com/common/oauth2/nativeclient, https://${exposeAPIURL}' --platform spa --grantAdminConsent  --multitenant --withSecret --uri api://${exposeAPIURL}/_appId_ --scopeName access_as_user --scopeAdminConsentDescription 'Access the application as Logged in User' --scopeAdminConsentDisplayName 'Access as the logged in User' --scopeConsentBy admins --output json`);
+    }
+    catch (err) {
+        console.error(`  ${chalk.red(err.stderr)}`);
+    }
+    return createdApp;
+}
+
+// For Production
+// const appName = 'Teams App Camp App June3-V1';
+//For Development
+const appName = 'Teams App Camp App June3-V1-DEV';
+const apiPermissions = 'https://graph.microsoft.com/User.Read';
+
+// //For Development
+const exposeAPIURL = 'b172-106-51-160-179.ngrok-free.app';
+// //For Production
+// const exposeAPIURL = 'teams-appcamp-june3.azurewebsites.net';
+
+const appCreated = await createAADApp(appName, apiPermissions, exposeAPIURL);
---
+++ demo-codes/graph-connector/Readme.md
@@ -0,0 +1,3 @@
+# About
+
+This is the repository which contains the artifacts used by Arjun Menon in Experts Live India for the session **Introduction to M365 Graph Connector - Get External Content to Microsoft 365 - Arjun**
