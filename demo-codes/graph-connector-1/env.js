71435bed352c9e643c3fb5fc81d272480c03aa27
Arjun Menon <arjun.umenon@gmail.com>
2024-02-16 01:14:49 +0530

    Working with original content

--- demo-codes/graph-connector-1/env.js
+++ demo-codes/graph-connector-1/env.js
@@ -1,11 +1,11 @@
 export const appInfo = {
-  "appId": "",
-  "objectId": "",
-  "tenantId": "",
+  "appId": "0df89e78-d1d9-40ea-9bfb-7c3de74b153c",
+  "objectId": "3fb1f2dd-b6ed-4af4-a917-23edb65db88a",
+  "tenantId": "095efa67-57fa-40c7-b7cc-e96dc3e5780c",
   "secrets": [
     {
-      "displayName": "",
-      "value": ""
+      "displayName": "Default",
+      "value": "6OT8Q~kAjcCWFWZjlHzrANA5C3q~zY__pwr7sbpJ"
     }
   ]
 };
